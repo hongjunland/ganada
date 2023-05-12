a0b6bd056dafb5554399a562a880d53ba6cff88a
Hongjun Lee <zunza96@gmail.com>
2023-05-12 15:47:19 +0900

    init

---
+++ src/main/java/example/ganada/GanadaApplication.java
@@ -0,0 +1,13 @@
+package example.ganada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GanadaApplication.class, args);
+    }
+
+}
