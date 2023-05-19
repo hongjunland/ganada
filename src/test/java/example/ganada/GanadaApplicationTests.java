b228f1e3a71135e68d6dc8d1238bf1b276967c49
Hongjun Lee <zunza96@gmail.com>
2023-05-19 14:26:32 +0900

    chore: gradle.yml 수정

---
+++ src/test/java/example/ganada/GanadaApplicationTests.java
@@ -0,0 +1,13 @@
+package example.ganada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GanadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
