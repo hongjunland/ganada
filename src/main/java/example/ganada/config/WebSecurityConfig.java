9c74f5c640138a64fa7b540a1e263ed5114884e1
Hongjun Lee <zunza96@gmail.com>
2023-05-19 16:25:54 +0900

    chore: application.yml ignore

---
+++ src/main/java/example/ganada/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package example.ganada.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
